5b75911534b5d2171473b164ec59cf8e4c191ede
RuilDizajn <49971485+RuilDizajn@users.noreply.github.com>
2023-07-23 22:20:22 -0400

    Update anbus.php

--- anbus.php
+++ anbus.php
@@ -1,27 +1,6 @@
 <?php
    $respuestas = array(
        "es un anbu asesino de Iwagakure.",
-       "es un anbu rastrero de Konohagakure.",
-       "es un fraca que nunca pasó de Gennin. ¡Avíspate! ",
-       ". Ninja médico de Kumogakure",
-       "pero si usted es un kage, señor.",
-       "es un anbu asesino de Amegakure.",
-       "es un anbu asesino de Kumogakure.",
-       "es un anbu asesino de Sunagakure.",
-       "es un anbu rastrero de Iwagakure.",
-       "es un anbu rastrero de Amegakure.",
-       "es un anbu rastrero de Kumogakure.",
-       "es un anbu rastrero de Sunagakure.",
-       ". Ninja médico de Iwagakure",
-       ". Ninja médico de Konohagakure",
-       ". Ninja médico de Kumogakure",
-       ". Ninja médico de Amegakure",
-       ". Ninja médico de Sunagakure",
-       "pero si usted es un kage, señor.",
-       "anbu espía de Sunagakure.",
-       "anbu espía de Amegakure.",
-       "anbu espía de Kumogakure.",
-       "anbu espía de Konohagakure.",
        "¡Ve a cocinar ramen, nunca fuiste a la escuela ninja!",
        "anbu espía de Iwagakure."
        );
