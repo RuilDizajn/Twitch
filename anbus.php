3ed547273da65d6dd8f90e497b5bf3cd4e6f3fd4
RuilDizajn <49971485+RuilDizajn@users.noreply.github.com>
2023-07-23 22:25:12 -0400

    Update anbus.php

--- anbus.php
+++ anbus.php
@@ -1,7 +1,6 @@
 <?php
    $respuestas = array(
        "es un anbu asesino de Iwagakure.",
-       "¡Ve a cocinar ramen, nunca fuiste a la escuela ninja!",
        "anbu espía de Iwagakure."
        );
    $random = array_rand($respuestas);
